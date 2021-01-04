85209d7bde36e29b6dd3f069b731356c1e13d71b
anna.henryson <anna.henryson@jeppesen.com>
2021-01-04 10:19:07 +0100

    fix

--- AdventOfCode/Day3/Day3.cs
+++ AdventOfCode/Day3/Day3.cs
@@ -11,32 +11,7 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode.Day3
 {
-  class Day3
-  {
-    static void Main(string[] args)
-    {
-      string[] data = File.ReadAllLines("../../Day3/InputData.txt");
-      int trees = 0;
-      int i = 0;
-      foreach (var row in data)
-      {
-        i += 3;
-        if (i >= row.Length)
-        {
-          i = (i - row.Length) * -1;
-        }
-        else if (row[i] == '#')
-        {
-          trees++;
-        }
-
-        
-      }
-      Console.WriteLine(trees);
-    }
-  }*/
-
-/*  public static class Day3
+ public static class Day3
   {
     static int r;
     static int d;
@@ -59,8 +34,7 @@ namespace AdventOfCode.Day3
         d += 2;
         if (input.Length <= d)
         {
-          Console.WriteLine($"Answer: {trees} trees");
-          Console.ReadLine();
+          Console.WriteLine(trees);
         }
 
         if (line.Length <= r)
