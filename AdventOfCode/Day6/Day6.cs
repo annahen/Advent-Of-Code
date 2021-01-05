87f46171795c9c374410a7f76cba94eba222fa5f
anna.henryson <anna.henryson@jeppesen.com>
2021-01-05 12:09:52 +0100

    day 6 part 1

---
+++ AdventOfCode/Day6/Day6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day6
+{
+  class Day6
+  {
+    static void Main(string[] args)
+    {
+      var data = ParseData();
+      List<int> allYeses = new List<int>();
+      allYeses.Add(FindYeses(data));
+      Console.WriteLine(allYeses.Sum());
+      Console.ReadLine();
+    }
+
+    private static int FindYeses(string[][] data)
+    {
+      int res = 0;
+      foreach (var item in data)
+      {
+        var s = String.Join("", item);
+        int count = s.Distinct().Count();
+        res += count;
+      }
+      return res;
+    }
+
+    private static string[][] ParseData()
+    {
+      var data = File.ReadAllText("../../Day6/InputData.txt");
+      return data.Split(new string[] {"\r\n\r\n"}, StringSplitOptions.RemoveEmptyEntries)
+        .Select(r => r.Replace("\r\n", " ").Split(' ')).ToArray();
+    }
+  }
+}
