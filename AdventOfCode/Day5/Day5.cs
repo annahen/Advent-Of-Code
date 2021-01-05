23d439cb6ce1be8e96a803987371b4c151ac0b56
anna.henryson <anna.henryson@jeppesen.com>
2021-01-05 09:38:51 +0100

    day 5

--- AdventOfCode/Day5/Day5.cs
+++ AdventOfCode/Day5/Day5.cs
@@ -1,91 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Day5
 {
   class Day5
   {
-    private static string[] airplaneRows = new string[127];
-    private static string[] currentSeatMapRows = new string[] { };
-    private static string[] airplaneColumns = new string[7];
-    private static string[] currentSeatMapColumns = new string[] { };
-    static List<Seat> seats = new List<Seat>();
     static void Main(string[] args)
     {
       var data = ParseData();
-      foreach (var boardingPass in data)
-      {
-        var seat = GetSeat(boardingPass);
-        seats.Add(seat);
-      }
-      seats.Sort();
+
+      List<int> seatIds = data
+        .Select(seat => Convert.ToInt32(ConvertToBinary(seat), 2))
+        .ToList();
+
+      seatIds.Sort();
+
+      Console.WriteLine(seatIds.Max()); //Part 1
+      Console.WriteLine(Enumerable.Range(seatIds.Min(), seatIds.Max()) //Part 2
+        .FirstOrDefault(id => !seatIds.Contains(id))
+        .ToString());
     }
 
-    private static Seat GetSeat(string boardingPass)
+    private static string ConvertToBinary(string seat)
     {
-      currentSeatMapRows = airplaneRows;
-      currentSeatMapColumns = airplaneColumns;
-      foreach (var character in boardingPass)
-      {
-        switch (character)
-        {
-          case 'F':
-            GetFirstHalfRow();
-            break;
-          case 'B':
-            GetSecondHalfRow();
-            break;
-          case 'L':
-            GetFirstHalfColumn();
-            break;
-          case 'R':
-            GetSecondHalfColumn();
-            break;
-        }
-      }
-      return new Seat(Convert.ToInt32(currentSeatMapRows), Convert.ToInt32(currentSeatMapColumns));
-    }
-
-    private static void GetSecondHalfColumn()
-    {
-      currentSeatMapColumns = currentSeatMapColumns.Skip(currentSeatMapColumns.Length / 2).ToArray();
-    }
-
-    private static void GetFirstHalfColumn()
-    {
-      currentSeatMapColumns = currentSeatMapColumns.Take(currentSeatMapColumns.Length / 2).ToArray();
-    }
-
-    private static void GetSecondHalfRow()
-    {
-      currentSeatMapRows = currentSeatMapRows.Skip(currentSeatMapRows.Length / 2).ToArray();
-    }
-
-    private static void GetFirstHalfRow()
-    {
-      currentSeatMapRows = currentSeatMapRows.Take(currentSeatMapRows.Length / 2).ToArray();
+      return seat
+          .Replace('F', '0')
+          .Replace('B', '1')
+          .Replace('L', '0').Replace('R', '1');
     }
 
     private static string[] ParseData()
     {
       var data = File.ReadAllText("../../Day5/InputData.txt");
       return data.Split(new [] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
-    }
-  }
-
-  class Seat
-  {
-    private int row;
-    private int column;
-    public Seat(int row, int column)
-    {
-      this.row = row;
-      this.column = column;
     }
   }
 }
